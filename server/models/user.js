bee8caf63e80215a7caad6fb9c3301a9e9382390
unknown <arunabeshc@gmail.com>
2018-06-07 06:54:11 +0530

    final commit on new folder

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,61 @@
-app.post('/users',(req,res)=>{
-  //console.log(req.body);
+const {mongoose}=require('../db/mongoose.js');
+const validator=require('validator');
+const jwt=require('jsonwebtoken');
+const _=require('lodash');
 
-  var body=_.pick(req.body,['email','password']);
-
-  var users=new user({
-    email:body.email,
-    password:body.password
-  });
-
-
-
-  users.save().then(()=>{
-    //res.send(200,`data saved successfully ,${JSON.stringify(doc,undefined,2)}`);
-
-    return users.generateAuthToken();
-
-  }).then((token)=>{
-    res.header('x-auth',token).status(200).send(users);
-  }).catch((e)=>{
-    res.send(400,`the fuck !! ${e}`);
-  });
+var userSchema=new mongoose.Schema({
+  email:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true,
+    unique:true,
+    validate: {
+      validator: (value) => {
+        return validator.isEmail(value);
+      }
+      ,
+      message: `{value} is not a valid email`
+    }
+  },
+  password:{
+    type:String,
+    required: true,
+    minLength: 6
+  },
+  tokens:[{
+    access: {
+      type:String,
+      required:true
+    },
+    token: {
+      type:String,
+      required:true
+    }
+  }]
 });
+
+userSchema.methods.toJSON=function(){
+  var user=this;
+  var userObject=user.toObject();
+
+  return _.pick(userObject,['_id','email']);
+}
+
+userSchema.methods.generateAuthToken=function() {
+  var user = this;
+  var access='auth';
+  var token=jwt.sign({_id: user._id.toHexString(), access},'abc123');
+
+  user.tokens=user.tokens.concat([{access,token}]);
+
+  return user.save().then(()=>{
+    return token;
+  })
+};
+
+var user=mongoose.model('user',userSchema);
+
+module.exports={
+  user:user
+}
